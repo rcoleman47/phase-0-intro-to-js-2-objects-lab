742277bdebbe8f9aa68ba9c2e62ca76f5f8d9fa4
ryanadams <rcoleman.adams@gmail.com>
2022-04-05 15:43:53 -0500

    commit tree 150ea5438fa284ee3ef09b63003caf949a89cb06

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ryan",
+    streetAddress: "421 Ranch Rd"
+};
+
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    let newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployeeList = {...employee};
+    delete newEmployeeList[key];
+    return newEmployeeList;
+};
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+};
+
